5549ca16f0c4b50544c658f135e7bb5d9ef06698
Noman Mumtaz <115913726+nomaannm@users.noreply.github.com>
2023-09-17 18:42:42 +0530

    Create HillWeight.java

---
+++ HillWeight.java
@@ -0,0 +1,22 @@
+@author NomanMumtaz
+
+public class HillWeight {
+    static int weightOfHill(int hillLevels , int weightOfPeak , int weightIncrement)
+    {
+        int weight = weightOfPeak ;
+
+        for(int i = 2 ; i <= hillLevels; i++)
+        {
+            weightOfPeak += weightIncrement ;
+            weight += (weightOfPeak * i); 
+        } 
+
+        return weight;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(weightOfHill(4, 2, 1));
+    }
+    
+}
