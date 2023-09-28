62ce00d1f0c74fbd848f6b85dfb345d97aee92b3
Noman Mumtaz <115913726+nomaannm@users.noreply.github.com>
2023-09-28 13:27:45 +0530

    Create StringSum.java

---
+++ StringSum.java
@@ -0,0 +1,33 @@
+/**
+ * @author NomanMumtaz
+ * 
+ * 
+ */
+
+public class StringSum {
+    // This function adds two numeric strings.
+    public static String stringSum(String num1 , String num2)
+    {
+       StringBuilder result = new StringBuilder();
+       int carry = 0 ;
+
+       int i = num1.length() - 1 , j = num2.length() - 1;
+       while( i >= 0 || j >= 0 || carry > 0 )
+       {
+        int digit1 = (i >= 0 ) ? num1.charAt(i--) - '0' : 0 ;
+        int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0 ;
+        int sum = digit1 + digit2 + carry ;
+        carry = sum /10 ;
+        sum %= 10 ;
+
+        result.insert(0,sum);
+       }
+       return result.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(stringSum("98276525121193226473", "2768671939389403"));
+    }
+    
+}
